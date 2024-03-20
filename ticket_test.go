2edda34bef18e76693367419f86419bdc3e69077
chotepurespirit <chovyzzz.nai@gmail.com>
2024-03-20 21:30:34 +0700

    go testing trick - testable - struct

--- ticket_test.go
+++ ticket_test.go
@@ -10,6 +10,8 @@ func TestTicketPrice(t *testing.T) {
 		want float64
 	}{
 		{name: "Free ticket when age is 0", age: 0, want: 0.0},
+		{"Free ticket when age is under 3", 3, 0.0},
+		{"Ticket is $15 when age is 4", 4, 15.0},
 	}
 
 	for _, tt := range tests {
@@ -21,75 +23,4 @@ func TestTicketPrice(t *testing.T) {
 			}
 		})
 	}
-
-	t.Run("should return 0 when age is 0", func(t *testing.T) {
-		want := 0.0
-		var age int = 0
-
-		got := Price(age)
-
-		if got != want {
-			t.Errorf("Price(0) = %f; want %f", got, want)
-		}
-	})
-	t.Run("Free ticket when age under 3", func(t *testing.T) {
-		want := 0.0
-		var age int = 3
-
-		got := Price(age)
-
-		if got != want {
-			t.Errorf("Price(3) = %f; want %f", got, want)
-		}
-	})
-	t.Run("Ticket $15 when age at 4 years old", func(t *testing.T) {
-		want := 15.0
-		var age int = 4
-
-		got := Price(age)
-
-		if got != want {
-			t.Errorf("Price(4) = %f; want %f", got, want)
-		}
-	})
-	t.Run("Ticket $15 when age is 15", func(t *testing.T) {
-		want := 15.0
-		var age int = 15
-
-		got := Price(age)
-
-		if got != want {
-			t.Errorf("Price(15) = %f; want %f", got, want)
-		}
-	})
-	t.Run("Ticket $30 when age over 15", func(t *testing.T) {
-		want := 30.0
-		var age int = 16
-
-		got := Price(age)
-
-		if got != want {
-			t.Errorf("Price(16) = %f; want %f", got, want)
-		}
-	})
-	t.Run("Ticket $30 when age is 50", func(t *testing.T) {
-		want := 30.0
-		var age int = 50
-
-		got := Price(age)
-
-		if got != want {
-			t.Errorf("Price(50) = %f; want %f", got, want)
-		}
-	})
-	t.Run("Ticket $5 when age is over 50", func(t *testing.T) {
-		want := 5.0
-		var age int = 51
-
-		got := Price(age)
-
-		if got != want {
-			t.Errorf("Price(51) = %f; want %f", got, want)
-		}
-	})
 }
